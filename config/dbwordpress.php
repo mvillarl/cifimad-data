a995da14055cb9a6806bf1c453e002592b567de1
Mario Villar <mvillar@yunbit.es>
2022-12-27 13:53:09 +0100

    Configuración base de datos nueva web y tienda 2022

--- config/dbwordpress.php
+++ config/dbwordpress.php
@@ -2,10 +2,18 @@
 
 $_dbhost = $_prod || $_ppe? 'rdbms.strato.de': 'localhost';
 
-return [
+/* Datos antiguos (CifiMad y Fanvención hasta 2022)
+ * return [
 	'class' => 'yii\db\Connection',
 	'dsn' => 'mysql:host='.$_dbhost.';dbname=DB3060401',
 	'username' => 'U3060401',
 	'password' => 'cifi159mad753',
+	'charset' => 'utf8',
+];*/
+return [
+	'class' => 'yii\db\Connection',
+	'dsn' => 'mysql:host='.$_dbhost.';dbname=dbs2637510',
+	'username' => 'dbu2416116',
+	'password' => '3KLupBDn7qX7y2m',
 	'charset' => 'utf8',
 ];
